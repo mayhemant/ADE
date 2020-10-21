6d9c3eacd5c12a08471e5c5c0586584e3bbb0f1a
Hemant Joshi <hd4207577@gmail.com>
2020-10-21 14:09:30 +0530

    Signout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/auth");
+const { signup, signin, signout } = require("../controllers/auth");
 const { runValidation } = require("../validators");
 const { userSignupValidator } = require("../validators/auth");
 const router = express.Router();
 
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/signin", signin);
+router.get("/signout", signout);
 module.exports = router;
